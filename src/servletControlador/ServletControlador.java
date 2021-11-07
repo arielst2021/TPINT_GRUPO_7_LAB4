abb73172d69726e8b16799af0bb4d590cd4ec11a
ArielTapia <arieltapia42@gmail.com>
2021-11-07 20:14:06 -0300

    actualizacionServletControlador

--- src/servletControlador/ServletControlador.java
+++ src/servletControlador/ServletControlador.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import entidades.Curso;
+import entidades.Estado;
 import entidades.Materia;
 
 //import com.sun.tools.javac.util.Convert;
@@ -28,8 +29,10 @@ import entidades.Profesor;
 import entidades.Profesor2;
 import entidades.Semestre;
 import negocio.NegocioCurso;
+import negocio.NegocioEstado;
 import negocio.NegocioProfesor;
 import negocioImpl.NegocioCursoImpl;
+import negocioImpl.NegocioEstadoImpl;
 import negocioImpl.NegocioProfesorImpl;
 
 @WebServlet("/ServletControlador")
@@ -106,37 +109,48 @@ public class ServletControlador extends HttpServlet {
 		// doGet(request, response);
 
 		// ####################################################
-		// S E S I O N - I N G R E S O - S A L I D A
+		// S E S I O N - L O G I N - L O G O U T
 		// ####################################################
 		// INGRESA EL USUARIO CON SU USER Y PASS
 		if (request.getParameter("btnIngresarUsuario") != null) {
-			String paginaDestino = "";
+			String paginaDestino = "index.jsp";
 			String userProfesor = request.getParameter("txtUsuario");
 			String passProfesor = request.getParameter("txtContrasenia");
 			//
 			NegocioProfesor NegocioProfesor = new NegocioProfesorImpl();
 			Profesor2 Profesor2 = NegocioProfesor.iniciarSesion(userProfesor, passProfesor);
 			//
-			if (Profesor2 != null) {
-				if (Profesor2.getPerfil().getId() == 1) {
-					// ESTABLECE UN ATRIBUTO EN LA SESIÓN PARA ALMACENAR INFORMACIÓN
-					// SOBRE EL PROFESOR CONECTADO
-					HttpSession session = request.getSession();
-					session.setAttribute("perfil", Profesor2);
-					paginaDestino = "administrador.jsp";
-				}
-				if (Profesor2.getPerfil().getId() == 2) {
-					// ESTABLECE UN ATRIBUTO EN LA SESIÓN PARA ALMACENAR INFORMACIÓN
-					// SOBRE EL PROFESOR CONECTADO
-					HttpSession session = request.getSession();
-					session.setAttribute("perfil", Profesor2);
-					paginaDestino = "profesor.jsp";
-				}
-			} else {
-				response.sendRedirect("index.jsp");
+			if (Profesor2 == null) {
+					RequestDispatcher dispatcher = request.getRequestDispatcher(paginaDestino);
+					dispatcher.forward(request, response);
 			}
-			RequestDispatcher dispatcher = request.getRequestDispatcher(paginaDestino);
-			dispatcher.forward(request, response);
+			else {
+				
+				try {
+					if (Profesor2.getPerfil().getId() == 1) {
+						// ESTABLECE UN ATRIBUTO EN LA SESIÓN PARA ALMACENAR INFORMACIÓN
+						// SOBRE EL PROFESOR CONECTADO
+						HttpSession session = request.getSession();
+						session.setAttribute("perfil", Profesor2);
+						paginaDestino = "administrador.jsp";
+					}
+					if (Profesor2.getPerfil().getId() == 2) {
+						// ESTABLECE UN ATRIBUTO EN LA SESIÓN PARA ALMACENAR INFORMACIÓN
+						// SOBRE EL PROFESOR CONECTADO
+						HttpSession session = request.getSession();
+						session.setAttribute("perfil", Profesor2);
+						paginaDestino = "profesor.jsp";
+					}
+					RequestDispatcher dispatcher = request.getRequestDispatcher(paginaDestino);
+					dispatcher.forward(request, response);
+					
+				} catch (Exception e) {
+					RequestDispatcher dispatcher = request.getRequestDispatcher(paginaDestino);
+					dispatcher.forward(request, response);
+				}
+			
+			}
+
 		}
 
 		// SALE EL USUARIO DE LA SESION
@@ -153,26 +167,45 @@ public class ServletControlador extends HttpServlet {
 		if (request.getParameter("btnObtenerAlumnosPorCurso") != null) {
 			
 			int MateriaId = Integer.parseInt(request.getParameter("txtMateriaId"));
+			String MateriaNombre = request.getParameter("txtMateriaNombre");			
 			int SemestreId = Integer.parseInt(request.getParameter("txtSemestreId"));
+			String SemestreNombre = request.getParameter("txtSemestreNombre");
 			Year Anio = Year.of(Integer.parseInt(request.getParameter("txtAnio")));
 			int Legajo = Integer.parseInt(request.getParameter("txtLegajo"));
 			
 			Materia Materia = new Materia();
 			Materia.setId(MateriaId);
+			Materia.setNombre(MateriaNombre);
 			
 			Semestre Semestre = new Semestre();
 			Semestre.setId(SemestreId);
+			Semestre.setNombre(SemestreNombre);
+			
+			// GUARDO LOS DATOS DEL CURSO EN UNA VARIABLE SESSION
+			Curso Curso = new Curso(Materia, Semestre, Anio);
+			HttpSession session = request.getSession();
+			session.setAttribute("DatosDelCurso", Curso);
+			//
+
 			
 			Profesor2 Profesor2 = new Profesor2();
 			Profesor2.setLegajo(Legajo);
 			
-			Curso Curso = new Curso(Materia, Semestre, Anio, Profesor2);			
-//			ArrayList<Curso> lista = NegocioCurso.ObtenerCursosPorLegajoProfesor(Curso);			
-//			request.setAttribute("listaCursos", lista);
-//
-//			RequestDispatcher rd = request.getRequestDispatcher("/listarCursosPorProfesor.jsp");
-//			rd.forward(request, response);
+			NegocioCurso NegocioCurso = new NegocioCursoImpl();
+			Curso = new Curso(Materia, Semestre, Anio, Profesor2);			
+			ArrayList<Curso> lista = NegocioCurso.ObtenerAlumnosPorCurso(Curso);
 			
+			request.setAttribute("listaAlumnosPorCursos", lista);
+
+			//OBTENGO LOS ESTADOS PARA CALIFICAR AL ALUMNO
+			NegocioEstado NegocioEstado = new NegocioEstadoImpl();
+			Curso = new Curso(Materia, Semestre, Anio, Profesor2);			
+			ArrayList<Estado> ListaEstados = NegocioEstado.obtenerEstados();
+			
+			request.setAttribute("listaEstados", ListaEstados);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/agregarNota.jsp");
+			rd.forward(request, response);			
 		}
 	}
 }
