090a6da56012acd04c7c3db77ba6a57e7ec8f857
ArielTapia <arieltapia42@gmail.com>
2021-11-07 20:41:35 -0300

    actualizacion
    Cuando se agrega un nuevo alumno en alumnos_cursos se le agrega POR DEFAULT estado número 3 qué significa 'Sin calificar'. Se insertaron datos en las tablas Estados ,cursos, alumnos y alumnos por curso.

--- Estados_Materias_Perfiles_Profesores_Provincias_Semestres.sql
+++ Estados_Materias_Perfiles_Profesores_Provincias_Semestres.sql
@@ -57,3 +57,32 @@ INSERT INTO `laboratorio4`.`profesores` (`pro_legajo`, `pro_dni`, `pro_nombre`, 
 INSERT INTO `laboratorio4`.`profesores` (`pro_legajo`, `pro_dni`, `pro_nombre`, `pro_apellido`, `pro_fechanac`, `pro_direccion`, `pro_provincia_id`, `pro_email`, `pro_telefono`, `pro_estado_id`, `pro_perfil_id`, `pro_usuario`, `pro_contrasenia`) VALUES ('2', '22450807', 'Marta', 'Arnau', '2075-05-06', 'Ituzaingo 9725', '1', 'marta@gmail.com', '555-1500', '1', '2', 'usuario', 'usuario');
 
 
+
+
+
+
+/* AGREGADO EL 7-11 */
+/* Cuando se agrega un nuevo alumno en alumnos_cursos se le agrega POR DEFAULT estado número 3 qué significa 'Sin calificar'. Se insertaron datos en las tablas Estados ,cursos, alumnos y alumnos por curso. 
+ */
+ 
+INSERT INTO `laboratorio4`.`estados` (`est_id`, `est_nombre`) VALUES ('3', 'Sin calificar');
+INSERT INTO `laboratorio4`.`estados` (`est_id`, `est_nombre`) VALUES ('4', 'Libre');
+INSERT INTO `laboratorio4`.`estados` (`est_id`, `est_nombre`) VALUES ('5', 'Regular');
+
+
+ALTER TABLE alumnos_cursos    
+    ALTER COLUMN axc_estado_id
+        SET DEFAULT 3
+
+
+INSERT INTO `laboratorio4`.`cursos` (`cur_materia_id`, `cur_semestre_id`, `cur_anio`, `cur_profesor_legajo`) VALUES ('1', '1', 2020, '2');
+INSERT INTO `laboratorio4`.`cursos` (`cur_materia_id`, `cur_semestre_id`, `cur_anio`, `cur_profesor_legajo`) VALUES ('2', '2', 2020, '2');
+INSERT INTO `laboratorio4`.`cursos` (`cur_materia_id`, `cur_semestre_id`, `cur_anio`, `cur_profesor_legajo`) VALUES ('3', '1', 2021, '2');
+INSERT INTO `laboratorio4`.`cursos` (`cur_materia_id`, `cur_semestre_id`, `cur_anio`, `cur_profesor_legajo`) VALUES ('4', '1', 2019, '2');
+
+
+
+INSERT INTO `laboratorio4`.`alumnos` (`alu_dni`, `alu_nombre`, `alu_apellido`, `alu_fechanac`, `alu_direccion`, `alu_provincia_id`, `alu_email`, `alu_telefono`, `alu_estado_id`) VALUES ('12316196', 'Cristian', 'Flores', '2000-06-22', 'Ruíz de Ulla', '8', 'alumno@email.com', '555-5555', '1');
+
+
+INSERT INTO `laboratorio4`.`alumnos_cursos` (`axc_materia_id`, `axc_semestre_id`, `axc_anio`, `axc_profesor_legajo`, `axc_alumno_legajo`) VALUES ('1', '1', 2020, '2', '1');
