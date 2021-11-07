7c45ee156f89dee72e225fa694afff968c3b9adb
ArielTapia <arieltapia42@gmail.com>
2021-11-07 20:16:46 -0300

    agregadoNegocioEstadoImpl

---
+++ src/negocioImpl/NegocioEstadoImpl.java
@@ -0,0 +1,18 @@
+package negocioImpl;
+
+import java.util.ArrayList;
+
+import dao.EstadoDao;
+import daoImpl.EstadoDaoImpl;
+import entidades.Estado;
+import negocio.NegocioEstado;
+
+public class NegocioEstadoImpl implements NegocioEstado {
+
+	EstadoDao dao = new EstadoDaoImpl();
+
+	@Override
+	public ArrayList<Estado> obtenerEstados() {
+		return dao.obtenerEstados();
+	}
+}
