177d2918f17d05286b622734ecc7f9dc2e3c42dd
ArielTapia <arieltapia42@gmail.com>
2021-11-18 20:08:53 -0300

    updateConexion

--- src/daoImpl/Conexion.java
+++ src/daoImpl/Conexion.java
@@ -7,12 +7,15 @@ import java.sql.SQLException;
 public class Conexion {
 	public static Conexion instancia;
 	private Connection connection;
+	//
+    public static final String URL = "jdbc:mysql://localhost:3306/laboratorio4?useSSL=false";
+    public static final String USER = "root";
+    public static final String PASS = "root";
 
 	private Conexion() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/laboratorio4?useSSL=false",
-					"root", "ckroozlife_669");
+			this.connection = DriverManager.getConnection(URL,USER,PASS);
 			this.connection.setAutoCommit(false);
 		} catch (Exception e) {
 			e.printStackTrace();
